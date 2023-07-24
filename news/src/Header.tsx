a2d442fc859e83fc0e1cc6191a3e5cbed30f5958
zi0ne <jiwon0331j@gmail.com>
2023-07-24 21:51:59 +0900

    feat: header 수정

--- news/src/Header.tsx
+++ news/src/Header.tsx
@@ -20,19 +20,32 @@ const HeaderBox = styled.header`
     justify-content: center;
     align-items: center;
     margin-top: 30px;
-    padding-bottom: 20px;
     padding-left: 5%;
-    max-height: 150px;
+    max-height: 250px;
     font-family: 'TTWanjudaedunsancheB';
     border-bottom: solid 2px #d6d6d6;
     
     div{
       display: flex;
+      position: fixed;
       justify-content: center;
       align-items: center;
       width: 300px;
+      height: 120px;
+      top: -45px;
+      margin-top: 50px;
       background-image: url('talk.png');
       background-size: cover;
+    }
+
+    h3{
+      margin-top: -10px;
+      margin-left: -10px;
+    }
+
+    h1{
+      margin-top: 90px;
+      font-size: 45px;
     }
 `;
 
