e19907f26e84b938de7a7ec173b0c484d92309b9
zi0ne <jiwon0331j@gmail.com>
2023-07-26 00:46:32 +0900

    feat: 카테고리 css

--- src/Category.tsx
+++ src/Category.tsx
@@ -6,36 +6,36 @@ interface TapProps {
   }
 
 const Category : React.FC = () => {
-    const [select,setSelect] = useState('')
+    const [select,setSelect] = useState('all')
 
     const categories = [
         {
             name: 'all',
-            text: '전체보기'
+            text: '전체'
         },
         {
             name: 'business',
-            text: '비즈니스'
+            text: '💰경제'
         },
         {
             name: 'science',
-            text: '과학'
+            text: '💡과학'
         },
         {
             name: 'entertainment',
-            text: '연예'
+            text: '🎨연예'
         },
         {
             name: 'sports',
-            text: '스포츠'
+            text: '⚽️스포츠'
         },
         {
             name: 'health',
-            text: '건강'
+            text: '🧬의학'
         },
         {
             name: 'technology',
-            text: '기술'
+            text: '🤖테크'
         }
     ];
 
@@ -46,15 +46,23 @@ const Category : React.FC = () => {
     
     return(
         <CategoryBox>
-            {categories.map(tap => (
-                <Tap
-                key={tap.name}
-                onClick = {() => handleClick(tap.name)}
-                selected = {select === tap.name}
-                >
-                {tap.text}
-                </Tap>
-            ))}
+            <Wrap>
+                {categories.map(tap => (
+                    <Tap
+                    key={tap.name}
+                    onClick = {() => handleClick(tap.name)}
+                    selected = {select === tap.name}
+                    >
+                    {tap.text}
+                    </Tap>
+                ))}
+                <Search>
+                <div className="inputSearch">
+                    <input placeholder="검색어를 입력하세요"/>
+                </div>
+                <div className="button">🔎</div>
+                </Search>
+            </Wrap>
         </CategoryBox>
     );
 }
@@ -62,13 +70,61 @@ const Category : React.FC = () => {
 const CategoryBox = styled.div`
     display: flex;
     flex-direction: row;
-    border-bottom: solid 2px #ffffff;
+    height: 70px;
+    justify-content: center;
+    align-items: center;
+    border-bottom: solid 2px #3d3d3d;
+    font-family: 'TTWanjudaedunsancheB';
+    font-size: 20px;
+`;
+
+const Wrap = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 70px;
+    width: 70%;
+    justify-content: space-around;
+    align-items: center;
+    font-family: 'TTWanjudaedunsancheB';
+    font-size: 20px;
 `;
 
 const Tap = styled.div<TapProps>`
     cursor: pointer;
     margin-right: 10px;
-    color: ${props => props.selected ? 'red' : 'black'};
+    color: ${props => props.selected ? '#ff6b00' : 'black'};
+    transition: font-size 0.2s ease;
+
+    &:hover {
+        font-size: 1.2em; 
+      }
+`;
+
+const Search = styled.div`
+    display: flex;
+    flex-direction: row;
+    cursor: pointer;
+
+    input{
+        width: 230px;
+        border:none;
+        margin-left: 5px;
+        padding: 8px 0px 0px 5px;
+    }
+    input:focus {
+        outline: none;
+      }
+    .inputSearch{
+        width: 250px;
+        border: solid 2px #ff6b00;
+        border-radius: 10px;
+    }
+
+    .button{
+        margin-right: -20px;
+        margin-left: 15px;
+        font-size: 25px;
+    }
 `;
 
 export default Category;
